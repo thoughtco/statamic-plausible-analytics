f0dc8b55d702dfba4837363d299e6074ef866864
Ryan Mitchell <ryan@thoughtcollective.com>
2024-08-19 12:32:19 +0100

    Update to laravel 10 approach to route controllers

--- routes/cp.php
+++ routes/cp.php
@@ -1,9 +1,12 @@
 <?php
 
-Route::get('/analytics', 'DashboardController@index')->name('plausible.index');
+use Jackabox\Plausible\Http\Controllers\Api;
+use Jackabox\Plausible\Http\Controllers\DashboardController;
 
-Route::get('/plausible/api/top-pages', 'Api\TopPagesController@fetch')->name('plausible.api.top-pages');
-Route::get('/plausible/api/top-referrers', 'Api\TopReferrersController@fetch')->name('plausible.api.top-referrers');
-Route::get('/plausible/api/top-browsers', 'Api\TopBrowsersController@fetch')->name('plausible.api.top-browsers');
-Route::get('/plausible/api/timeseries', 'Api\TimeseriesController@fetch')->name('plausible.api.timeseries');
-Route::get('/plausible/api/aggregates', 'Api\AggregatesController@fetch')->name('plausible.api.aggregates');
+Route::get('/analytics', [DashboardController::class, 'index'])->name('plausible.index');
+
+Route::get('/plausible/api/top-pages', [Api\TopPagesController::class, 'fetch'])->name('plausible.api.top-pages');
+Route::get('/plausible/api/top-referrers', [Api\TopReferrersController::class, 'fetch'])->name('plausible.api.top-referrers');
+Route::get('/plausible/api/top-browsers', [Api\TopBrowsersController::class, 'fetch'])->name('plausible.api.top-browsers');
+Route::get('/plausible/api/timeseries', [Api\TimeseriesController::class, 'fetch'])->name('plausible.api.timeseries');
+Route::get('/plausible/api/aggregates', [Api\AggregatesController::class, 'fetch'])->name('plausible.api.aggregates');
